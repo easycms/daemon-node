68fd5e570783079dfc2323bbbd341d5a2381c7d3
余重桦 <yuchonghua@163.com>
2018-11-08 20:51:04 +0800

    Base commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'html',
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {},
+  globals: {
+  }
+}
